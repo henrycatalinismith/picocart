45098ee31181973e437f662f6ddfa237fc5d93d1
Henry Catalini Smith <henry@catalinismith.se>
2018-12-23 21:03:19 +0000

    Set up some colors and stuff

---
+++ components/header.js
@@ -0,0 +1,14 @@
+import colors from "../colors"
+
+export default () => [
+  <h1>picocart</h1>,
+
+  <style jsx>{`
+    h1 {
+      font-family: sans-serif;
+      margin: 0;
+      background-color: ${colors[14]};
+    }
+  `}</style>
+]
+
