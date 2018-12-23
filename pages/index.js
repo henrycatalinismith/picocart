cad07a050819fea6d680f89f27386283586aee66
Henry Catalini Smith <henry@catalinismith.se>
2018-12-23 17:58:05 +0000

    Delete custom page (was just an experiment I guess)

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,12 @@
-export default () => (
-  <div>picocart</div>
-)
+export default () => [
+  <div>picocart</div>,
+
+  <style jsx>{`
+    div {
+      font-family: sans-serif;
+      margin: 0;
+      background-color: red;
+    }
+  `}</style>
+]
 
