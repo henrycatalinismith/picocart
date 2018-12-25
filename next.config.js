beabd7e37d42e23605920071f220f49e8b3808cc
Henry Catalini Smith <henry@catalinismith.com>
2018-12-25 17:12:21 +0100

    Set up bundle analyzer

--- next.config.js
+++ next.config.js
@@ -2,9 +2,22 @@ require("dotenv").config()
 
 const path = require("path")
 const Dotenv = require("dotenv-webpack")
+const withBundleAnalyzer = require("@zeit/next-bundle-analyzer")
 
-module.exports = {
-  webpack: config => {
+const nextConfig = {
+  analyzeServer: ["server", "both"].includes(process.env.BUNDLE_ANALYZE),
+  analyzeBrowser: ["browser", "both"].includes(process.env.BUNDLE_ANALYZE),
+  bundleAnalyzerConfig: {
+    server: {
+      analyzerMode: "static",
+      reportFilename: "../bundles/server.html"
+    },
+    browser: {
+      analyzerMode: "static",
+      reportFilename: "../bundles/client.html"
+    }
+  },
+  webpack (config) {
     config.plugins = config.plugins || []
 
     config.plugins = [
@@ -18,3 +31,5 @@ module.exports = {
     return config
   }
 }
+
+module.exports = withBundleAnalyzer(nextConfig)
