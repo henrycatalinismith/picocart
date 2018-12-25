954babaa324e78646407621113d1ad5299e4033a
Henry Catalini Smith <henry@catalinismith.se>
2018-12-25 17:01:35 +0100

    Set up Redux

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,35 @@
+import { connect } from "react-redux"
 import Document from "../components/document"
 import Header from "../components/header"
 import colors from "../colors"
 
-export default () => (
-  <Document>
-    <Header />
-    <p>index</p>
-    <p>{process.env.LOL}</p>
-    <style jsx>{`
-      p {
-        color: ${colors[0]};
-        font-weight: 900;
-      }
-    `}</style>
-  </Document>
-)
+class Index extends React.Component {
+  static getInitialProps ({ reduxStore, req }) {
+    const isServer = !!req
+    console.log(isServer)
+    return {}
+  }
 
+  componentDidMount () {
+    console.log("componentDidMount!")
+  }
+
+  render() {
+    return (
+      <Document>
+        <Header />
+        <p>index</p>
+        <p>{process.env.LOL}</p>
+        <style jsx>{`
+          p {
+            color: ${colors[0]};
+            font-weight: 900;
+          }
+        `}</style>
+      </Document>
+    )
+  }
+}
+
+
+export default connect()(Index)
