2ffb10fbe954b4ca025c9400f3c73e314a5572bf
Henry Catalini Smith <henry@catalinismith.se>
2018-12-26 21:19:33 +0100

    Add a cute little floppy

---
+++ components/floppy.js
@@ -0,0 +1,36 @@
+import colors from "../colors"
+
+
+
+
+export default () => (
+  <div className="floppy">
+    <svg className="floppy__svg" viewBox="0 0 16 16">
+      <path d="M0,0 C0,0 14.1624756,4.84345139e-16 14.0817871,0 C14.0010986,-4.84345139e-16 14.0130005,1.99951172 14.0130005,1.99951172 L16,1.99951172 L16,16 L0,16 L0,0 Z" id="Case" fill="#00AAFF"></path>
+      <rect id="Cover" fill="#C2C3C7" x="4" y="1" width="8" height="5"></rect>
+      <rect id="Window" fill="#00AAFF" x="9" y="2" width="2" height="3"></rect>
+      <rect id="Label" fill="#FFF1E7" x="3" y="7" width="10" height="7"></rect>
+      <g transform="translate(-1.000000, -1.000000)" stroke="#000000" stroke-linecap="square">
+        <path d="M1.5,1.5 L14.5,1.5" id="Line"></path>
+        <path d="M14.5,3.5 L14.5,1.5" id="Line"></path>
+        <path d="M16.5,3.5 L14.5,3.5" id="Line"></path>
+        <path d="M1.5,16.5 L16.5,16.5" id="Line"></path>
+        <path d="M1.5,2.5 L1.5,15.5" id="Line"></path>
+        <path d="M16.5,16.5 L16.5,3.5" id="Line"></path>
+      </g>
+    </svg>
+
+    <style jsx>{`
+      .floppy {
+        height: 23px;
+        width: 23px;
+        padding: 4px 2px;
+      }
+
+      .floppy {
+      }
+    `}</style>
+
+  </div>
+)
+
