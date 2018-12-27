cd10e88094eebb95ccf7d57bf10a6b202e0862da
Henry Catalini Smith <henry@catalinismith.se>
2018-12-27 20:40:28 +0100

    Put stage component into cart edit page

--- pages/cart.js
+++ pages/cart.js
@@ -1,6 +1,7 @@
 import { connect } from "react-redux"
 import Document from "../components/document"
 import Header from "../components/header"
+import Stage from "../components/stage"
 import Toolshed from "../components/toolshed"
 import colors from "../colors"
 
@@ -19,6 +20,7 @@ class Cart extends React.Component {
     return (
       <Document title="cart editor">
         <Header />
+        <Stage height={256}  />
         <Toolshed height={256} />
       </Document>
     )
