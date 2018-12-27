071000a257a92385ee2c36c2f343def8e738b1a4
Henry Catalini Smith <henry@catalinismith.se>
2018-12-27 17:38:29 +0100

    More storybook

--- components/header.js
+++ components/header.js
@@ -2,17 +2,18 @@ import Flop from "./flop"
 import Wordmark from "./wordmark"
 import colors from "../colors"
 
-export default () => (
+export default ({ bg = colors[14], height = 32 }) => (
   <div className="header">
     <Flop />
     <Wordmark />
 
     <style jsx>{`
       .header {
-        background-color: ${colors[14]};
-        height: 32px;
+        background-color: ${bg};
+        height: ${height}px;
         display: flex;
         justify-content: center;
+        width: 100%;
       }
     `}</style>
   </div>
