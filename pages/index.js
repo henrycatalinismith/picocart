fbee633231e587f82ef2e951268dfd3534f84220
Henry Catalini Smith <henry@catalinismith.com>
2019-01-01 20:19:32 +0100

    good enough for now

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,10 @@
+import PropTypes from "prop-types"
+import _ from "lodash"
 import { connect } from "react-redux"
 import Link from "next/link"
 import Document from "../components/document"
+import Grid from "../components/grid"
+import Cart from "../components/cart"
 import Header from "../components/header"
 import actions from "../actions"
 import colors from "../colors"
@@ -10,19 +14,50 @@ class Index extends React.Component {
     return {}
   }
 
+  static mapStateToProps = state => ({
+    carts: state.carts,
+  })
+
+  static mapDispatchToProps = (dispatch, props) => ({
+  });
+
+  static propTypes = {
+    carts: PropTypes.object,
+  }
+
   render() {
+    const { carts } = this.props
+
     return (
       <Document>
         <Header />
-        <p>
-          <Link href="/cart">
-            <a>Edit</a>
-          </Link>
-        </p>
+
+        <div className="library">
+          <Grid n={4} w={128}>
+            {_.keys(carts).map((e, i) => (
+              <Cart
+                key={carts[e].id}
+                bg={colors[1]}
+                cart={carts[e]}
+                size={96}
+              />
+            ))}
+          </Grid>
+        </div>
+
+        <style jsx>{`
+          .library {
+            height: calc(100vh - 32px - 8px - 8px);
+            padding: 8px;
+          }
+        `}</style>
+
       </Document>
     )
   }
 }
 
-
-export default connect()(Index)
+export default connect(
+  Index.mapStateToProps,
+  Index.mapDispatchToProps
+)(Index)
