db5a4a2fd0a15a6d3906e16d477c3e709ddad1ce
Henry Catalini Smith <henry@catalinismith.se>
2019-01-02 13:44:08 +0100

    Attempt ios viewport hack

---
+++ components/bucket.js
@@ -0,0 +1,43 @@
+import React from "react"
+import PropTypes from "prop-types"
+import colors from "../colors"
+
+class Bucket extends React.PureComponent {
+  static propTypes = {
+    width: PropTypes.number,
+    height: PropTypes.number,
+    thickness: PropTypes.number,
+  }
+
+  static defaultProps = {
+    width: undefined,
+    height: undefined,
+    thickness: 4,
+  }
+
+  render() {
+    const {
+      children,
+      height,
+      thickness,
+      width,
+    } = this.props
+
+    return (
+      <div className="bucket">
+        {children}
+        <style jsx>{`
+          .bucket {
+            border-color: ${colors[14]};
+            border-style: solid;
+            border-right-width: ${thickness}px;
+            border-bottom-width: ${thickness}px;
+            border-left-width: ${thickness}px;
+          }
+        `}</style>
+      </div>
+    )
+  }
+}
+
+export default Bucket
